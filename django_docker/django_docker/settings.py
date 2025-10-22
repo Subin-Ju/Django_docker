9a41eaeaca9cff2eb5ffe179004e92c68125857f
Subin-Ju <jusubin6856@naver.com>
2025-10-22 11:13:57 +0900

    ALLOWED_HOSTS

--- django_docker/django_docker/settings.py
+++ django_docker/django_docker/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['localhost','15.165.20.100']
 
 # Application definition
 
